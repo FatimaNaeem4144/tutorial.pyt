3fbacabe46613e2c3d48daa6bd6d3452a8302306
FatimaNaeem4144 <114748587+FatimaNaeem4144@users.noreply.github.com>
2022-09-30 06:31:39 -0700

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,18 @@
+print("WELCOME TO THE DICTIONARY WORLD")
+d1 = {"Bts":"Bangtan Boys:the seven normal boys from korea",
+      "Ramen":"the eating food",
+      "Army":"the true and genuine fans of bts",
+      "Tears":"the liquid which comes from your eyes whenever you cry. ",
+      "Mutable":"can change",
+      "Patriot":"a country's lover",
+      "Affection":"love",
+      "Friend":"a person who is honest with you",
+      "Cat":"a cute animal",
+      "Comb":"a plastic item for set your hair"}
+print("Enter your word: ")
+x = input()
+y = x.capitalize()
+print("The meaning of the word is",d1[y])
+
+
+print("THANKS FOR USING THE DICTIONARY WORLD")
